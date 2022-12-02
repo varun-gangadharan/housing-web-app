d83f377db886c40ab5163b66f5204659bd7e23ec
Matthew Jakimovski <mjakim2@illinois.edu>
2022-12-01 18:19:29 -0600

    Revert "Changed secrets.js"
    This reverts commit 91dc1cb27e0c7d8c88d2635e1f09626e8718b24c.

--- config/secrets.js
+++ config/secrets.js
@@ -1,5 +1,4 @@
 module.exports = {
     token: "connection string",
-    mongo_connection: "mongodb+srv://student:test@Cluster0.rvezzrd.mongodb.net/test"
-    //"<input connection string here and add this file to .gitignore>"
+    mongo_connection: "<input connection string here and add this file to .gitignore>"
 }
