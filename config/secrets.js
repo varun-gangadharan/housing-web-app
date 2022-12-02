6c54ff49c06299130f822fdf3f516c76567cd954
Matthew Jakimovski <mjakim2@illinois.edu>
2022-12-01 18:27:21 -0600

    Changed secrets.js & .gitignore

--- config/secrets.js
+++ config/secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
     token: "connection string",
-    mongo_connection: "<input connection string here and add this file to .gitignore>"
+    mongo_connection: "mongodb+srv://student:test@Cluster0.rvezzrd.mongodb.net/test"
 }
