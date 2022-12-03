16ff70d1afb422cfbd947d8b12b4661fd76a6544
Alexander Murphy <therealalexandermurphy@gmail.com>
2022-12-03 16:19:40 -0600

    Prototype Sign Up

--- front_end/front_end/src/Components/SignUp.js
+++ front_end/front_end/src/Components/SignUp.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import "./SignUp.css"
+import "../App.css"
 
 export default function SignUp() {
     const [firstName, setFirstName] = useState("");
@@ -12,10 +14,10 @@ export default function SignUp() {
             <h2>Sign up for an account now!</h2>
             <div>
                 <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="First name"></input>
-                <input type="text" value={secondName} onChange={(e) => setFirstName(e.target.value)} placeholder="Second name"></input>
-                <input type="text" value={email} onChange={(e) => setFirstName(e.target.value)} placeholder="Email"></input>
-                <input type="text" value={password} onChange={(e) => setFirstName(e.target.value)} placeholder="Password"></input>
-
+                <input type="text" value={secondName} onChange={(e) => setSecondName(e.target.value)} placeholder="Second name"></input>
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
+                <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"></input>
+                <button>Sign Up!</button>
             </div>
         </div>
     );
