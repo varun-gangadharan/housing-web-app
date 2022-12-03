5a4d4fe31f86a01b3cb1b9cd16416735e7233d39
Varun Gangadharan <varunpg2@illinois.edu>
2022-12-03 16:42:10 -0600

    ting

---
+++ front_end/front_end/src/Components/Profile.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import "../App.css"
+
+export default function UserProfile(){
+    return (
+        <div>
+            <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+            />
+            <div className="card">
+                <img src="img.jpg" alt="John" style={{ width: "100%" }} />
+                <h1>John Doe</h1>
+                <p className="details">example &amp; example, Example</p>
+                <p>phone number</p>
+        <p>
+          <button>Contact</button>
+        </p>
+      </div>
+    </div>
+    );
+}
+
