7232d5f83cebcd23b9909cf9ab1a9276e89e1080
Varun Gangadharan <varunpg2@illinois.edu>
2022-12-03 16:03:59 -0600

    ting

--- front_end/front_end/src/Components/SignIn.js
+++ front_end/front_end/src/Components/SignIn.js
@@ -1,1 +1,21 @@
 import './App.css';
+import React, { Component } from 'react';
+import { auth, googleAuthProvider } from '../../firebase.js';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import axios from 'axios';
+import './Signin.scss';
+import { Redirect } from 'react-router-dom'
+import { Message } from 'semantic-ui-react'
+
+class Signin extends Component{
+    state = {
+        isSignedIn: false,
+        willRedirect: false
+    };
+    handleSuccessfulSignIn = () => {
+		this.setState({willRedirect: true});
+	}
+}
+
+
+export default Signin;
