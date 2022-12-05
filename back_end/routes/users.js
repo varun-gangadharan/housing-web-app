6613f674fafef18aa0e45162f5dfb690f55145c0
Milosz11 <milosz11@yahoo.com>
2022-12-04 18:39:10 -0600

    Implement functionality for user routes: GET /users and POST /users

--- back_end/routes/users.js
+++ back_end/routes/users.js
@@ -6,26 +6,72 @@ module.exports = function (router) {
 
     // GET /users
     usersRoute.get(async (req, res) => {
-        // ...
+        const skip = req.query.skip === undefined ? 0 : parseInt(req.query.skip)
+        const limit = req.query.limit === undefined ? null : parseInt(req.query.limit)
+        const query = req.query.where === undefined ? undefined : JSON.parse(req.query.where)
+        const sort = req.query.sort === undefined ? undefined : JSON.parse(req.query.sort)
+
+        try {
+            const users = await User.find(query).sort(sort).skip(skip).limit(limit)
+            return res.status(200).json({ message: 'Retrieved list of users', data: users })
+        } catch (err) {
+            return res.status(500).json({ message: 'Server error', data: '' })
+        }
+
     })
 
     // POST /users
     usersRoute.post(async (req, res) => {
-        // ...
+        if (req.body.email === undefined) {
+            return res.status(400).json({ message: 'Email missing', data: '' })
+        }
+        if (req.body.firstName === undefined) {
+            return res.status(400).json({ message: 'First name missing', data: '' })
+        }
+        if (req.body.lastName === undefined) {
+            return res.status(400).json({ message: 'Last name missing', data: '' })
+        }
+        if (req.body.password === undefined) {
+            return res.status(400).json({ message: 'Password missing', data: '' })
+        }
+        if (req.body.phoneNumber === undefined) {
+            return res.status(400).json({ message: 'Phone number missing', data: '' })
+        }
+
+        const user = new User({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: req.body.password,
+            phoneNumber: req.body.phoneNumber
+        })
+        
+        try {
+            const exists = await User.exists({ email: user.email })
+            if (exists) {
+                return res.status(400).json({ message: 'User with email ' + user.email + ' already exists', data: '' })
+            }
+
+            const newUser = await user.save()
+            return res.status(201).json({ message: 'User successfully created', data: newUser })
+        } catch (err) {
+            return res.status(500).json({ message: 'Server error', data: '' })
+        }
+
     })
 
     // GET /users/:id
     usersIDRoute.get(async (req, res) => {
-        // ...
+        const userId = req.params.id
     })
 
     // PUT /users/:id
     usersIDRoute.put(async (req, res) => {
-        // ...
+        const userId = req.params.id
     })
 
     // DELETE /users/:id
     usersIDRoute.delete(async (req, res) => {
-        // ...
+        const userId = req.params.id
     })
 }
