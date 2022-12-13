f6588447b8970d35866d8baf58c2b907f5056972
Milosz <mdabro6@illinois.edu>
2022-12-12 19:29:42 -0600

    Fix mongo connection bug

--- config/secrets.js
+++ config/secrets.js
@@ -1,4 +1,5 @@
 module.exports = {
     token: "connection string",
-    mongo_connection: "mongodb+srv://student:test@Cluster0.rvezzrd.mongodb.net/test"
+    // mongo_connection: "mongodb+srv://student:test@Cluster0.rvezzrd.mongodb.net/test"
+    mongo_connection: "mongodb+srv://student:test@cluster0.180lac4.mongodb.net/?retryWrites=true&w=majority"
 }
