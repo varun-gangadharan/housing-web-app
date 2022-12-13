8c23e6e0662e0e27b59a2d0b5e94a454e6848a47
Varun Gangadharan <varunpg2@illinois.edu>
2022-12-12 19:30:13 -0600

    slight changes

--- front_end/front_end/src/Components/Details.js
+++ front_end/front_end/src/Components/Details.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import "../App.css"
 import './Details.css';
+import Listing from "./Listing";
 import {
     createBrowserRouter,
     RouterProvider,
     Route,
     Link,
   } from "react-router-dom";
+import { getListings} from "../api";
 
   export default function Details() {
-    return (
-      <div className="ting">
-        <div>
-          <h2>VAMM</h2>
-        </div>
+
+    return(
+      <div>
+          <div className="nav_bar">
+              <h2>VAMM</h2>
+              <Link to="sign-in">Sign In</Link>
+          </div>  
+          
       </div>
-    );
+  );
   }
