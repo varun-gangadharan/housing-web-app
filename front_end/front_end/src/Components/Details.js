dcc8932eb0327f7d6ca4e3b1f7e3ed40ec73649c
Varun Gangadharan <varunpg2@illinois.edu>
2022-12-12 19:45:30 -0600

    ting

--- front_end/front_end/src/Components/Details.js
+++ front_end/front_end/src/Components/Details.js
@@ -18,6 +18,33 @@ import { getListings} from "../api";
               <h2>VAMM</h2>
               <Link to="sign-in">Sign In</Link>
           </div>  
+          <div>
+            <h1>Apartment for Rent</h1>
+            <p>
+        This beautiful apartment is located in the heart of the city, with easy access to public transportation, shops, and restaurants.
+            </p>
+            <h3>Features</h3>
+            <ul>
+          <li>2 bedrooms</li>
+          <li>1 bathroom</li>
+          <li>Fully equipped kitchen</li>
+          <li>Balcony with city views</li>
+          <li>On-site laundry facilities</li>
+            </ul>
+            <h2>Details</h2>
+          <p> Price: $1,500 per month
+          Security deposit: $1,000
+          Available: January 1st, 2023
+          </p>
+          <h3>Contact</h3>
+      <p>
+        Please contact us for more information or to schedule a showing.
+      </p>
+      <p>
+        Phone: 555-555-5555
+        Email: info@apartmentrental.com
+      </p>
+          </div>
           
       </div>
   );
