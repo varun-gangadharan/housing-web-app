588ec88aa64d55e5c431aab61c29964ffe9cf417
Alexander Murphy <therealalexandermurphy@gmail.com>
2022-12-12 20:49:09 -0600

    fix navbar

--- front_end/front_end/src/Components/SignIn.js
+++ front_end/front_end/src/Components/SignIn.js
@@ -19,12 +19,14 @@ export default function SignIn() {
   const [status, setStatus] = useState("");
 
   return(
+    <div>
+      <div className="nav_bar">
+        <Link to="../">Home</Link>
+        <Link to="../sign-up">Sign Up</Link>
+      </div>
       <div className="login">
           <h1>VAMM Housing</h1>
-          <div className="nav_bar">
-                <Link to="home-page">Home</Link>
-                <Link to="sign-up">Sign Up</Link>
-            </div>
+
           <h2>Sign in now!</h2>
           <div>
               <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
@@ -32,6 +34,7 @@ export default function SignIn() {
               <button onClick={() => login(email,password).then((v) => setStatus((v.body == {}) ? "Login unsuccessful" : "Logged in successfully"))}>Sign In!</button>
               <p>{status}</p>
           </div>
+      </div>
       </div>
   );
 }
